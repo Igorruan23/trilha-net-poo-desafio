f2258c55a7612b1c47034933bdbf900bdf8f29b0
Igor Ruan <115436494+Igorruan23@users.noreply.github.com>
2025-05-20 10:20:21 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "12345", modelo: "android nokia", imei: "123123123123", memoria: 2);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("whatsapp");
+nokia.ReceberLigacao();
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone(numero: "123456", modelo: "10", imei: "1231231321231231", memoria: 6);
+iphone.Ligar();
+iphone.InstalarAplicativo("Reddit");
+iphone.ReceberLigacao();
